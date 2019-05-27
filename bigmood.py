1425e3b78e23ba69c109e94b2e89c14a494c775f
Ptyler21 <ptylerjr2@gmail.com>
2019-05-27 10:43:10 -0400

    basic implementation of the calculator class

--- bigmood.py
+++ bigmood.py
@@ -3,33 +3,51 @@ import string
 import logging
 import sys
 
-logging.basicConfig(level=logging.DEBUG,filename="examplelog")
-def addOperation(x,y):
-    addition = x + y
-    return addition
+logging.basicConfig(level=logging.DEBUG,filename="example.log")
 
-def subOperation(x,y):
-    subtraction = x - y
-    return subtraction
+class calculator:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+        self.choice = int(input("Please give me choice: "))
+        logging.debug("Operation Created: x = {} and y={}".format(self.x,self.y))
 
-def multiplicationOperation(x,y):
-    multiply = x * y
-    return multiply
+    def addition(self):
+        additionOperation = self.x + self.y
+        logging.debug("adding {} + {} , expected result = {}".format(self.x,self.y,additionOperation))
+        return additionOperation
 
-def divisionOperation(x,y):
-    divide = x/y
-    return divide
+    def subtraction(self):
+        subtractionOperation = self.x - self.y
+        logging.debug("subtracting {} - {}, expected result = {}".format(self.x,self.y,subtractionOperation))
+        return subtractionOperation
+
+    def division(self):
+        divisionOperation = self.x / self.y
+        logging.debug("dividing {} / {}, expected result = {}".format(self.x,self.y,divisionOperation))
+        return divisionOperation
+
+    def mulitply(self):
+        multiplyOperation = self.x * self.y
+        logging.debug("multiplying {} * {}, expected result = {}".format(self.x,self.y,multiplyOperation))
+        return multiplyOperation
+
+    def printoption(self):
+        print(self.choice)
+        logging.debug("user chose {}".format(self.choice))
+        if int(1) == self.choice:
+            print(self.addition())
+        elif int(2) == self.choice:
+            print(self.subtraction())
+        elif int(3) == self.choice:
+            print(self.division())
+        elif int(4) == self.choice:
+            print(self.mulitply())
+        else:
+            print("Non-valid option")
+            sys.exit(0)
 
 
-print("please pick you operation")
-print("1. Add")
-print("2. Subtract")
-print("3. Multiply")
-print("4. Divide")
+tree = calculator(3,4)
 
-try:
-    userInput = int(input("please input a number: ").strip())
-except ValueError:
-    logging.DEBUG("exiting system with input {}".format(userInput))
-    
-
+tree.printoption()
